0b0ebdb7e24920060bd39603c1167f4dfda181a8
harvace <ITISAasta@gmail.com>
2018-02-27 23:16:12 -0500

    gemma conductive fabric

--- sketches/flora_conductive_fabric/flora_conductive_fabric.ino
+++ sketches/flora_conductive_fabric/flora_conductive_fabric.ino
@@ -1,18 +1,17 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor   cs_9_10 = CapacitiveSensor(9,10);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
-Adafruit_FloraPixel strip = Adafruit_FloraPixel(1);
+CapacitiveSensor   cs = CapacitiveSensor(9,10);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
  
 void setup()                    
 {
-   cs_9_10.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs.set_CS_AutocaL_Millis(0xFFFFFFFF);
    Serial.begin(9600);
 }
  
 void loop()                    
 {
     long start = millis();
-    long total1 =  cs_9_10.capacitiveSensor(30);
+    long total1 =  cs.capacitiveSensor(30);
     
     Serial.println(total1);                  
  
